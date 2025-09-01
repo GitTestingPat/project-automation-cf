721d0146bbddc1301015647348b7d9e603f34baa
Patricio Andrade Gallardo <patricioandradeh@gmail.com>
2025-09-01 13:29:58 -0400

    refactor: Renombrar test_flights_api.py a test_flight_search_nyc_lon.py para alinear con TC-API-15

--- api_tests/test_flight_search_nyc_lon.py
+++ api_tests/test_flight_search_nyc_lon.py
@@ -1,13 +1,18 @@
 import requests
 import pytest
 
+'''
+Caso de prueba: TC-API-15: Buscar vuelos NYC -> LON
+Objetivo: Verificar que se puedan buscar vuelos entre dos ciudades usando parámetros válidos.
+'''
+
 BASE_URL = "https://cf-automation-airline-api.onrender.com"
 
 def test_list_airports():
-    # Probemos un endpoint que sí funciona
+    # Probar un endpoint que funciona
     response = requests.get(f"{BASE_URL}/airports/")
 
-    # Verificamos que la respuesta sea 200
+    # Verificar que la respuesta sea 200
     assert response.status_code == 200
 
     # Convertimos a JSON
