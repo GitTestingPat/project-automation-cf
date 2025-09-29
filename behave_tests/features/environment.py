1a051f2386315df85d7a09bb0002efe4d323fd80
Patricio Andrade Gallardo <patricioandradeh@gmail.com>
2025-09-29 16:26:42 -0300

    modifica pruebas BDD y workflow de GITHUB

--- behave_tests/features/environment.py
+++ behave_tests/features/environment.py
@@ -1,14 +1,25 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 import uuid
 
 def before_scenario(context, scenario):
     """Se ejecuta antes de cada escenario"""
     print("✅ before_scenario ejecutado")
     options = Options()
-    context.driver = webdriver.Chrome(options=options)
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+
+    # Usa webdriver-manager para obtener el driver automáticamente
+    service = Service(ChromeDriverManager().install())
+    context.driver = webdriver.Chrome(service=service, options=options)
     context.driver.implicitly_wait(10)
+
 
 def after_scenario(context, scenario):
     """Se ejecuta después de cada escenario"""
