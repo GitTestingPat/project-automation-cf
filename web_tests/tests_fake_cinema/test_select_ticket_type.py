15824833c8ba4e04e9c36af0be1d510cd2566a21
Patricio Andrade Gallardo <patricioandradeh@gmail.com>
2025-10-08 20:16:08 -0300

    Ajusta pruebas web

--- web_tests/tests_fake_cinema/test_select_ticket_type.py
+++ web_tests/tests_fake_cinema/test_select_ticket_type.py
@@ -19,7 +19,7 @@ def test_confirm_ticket_type(driver):
         print("[DEBUG] Navegando a detalle de Jurassic World...")
         home_page.navigate_to_movie_detail(home_page.JURASSIC_WORLD_DETAIL_BUTTON)
         print("[DEBUG] Seleccionando fecha")
-        home_page.select_date("8") # Ajustar según disponibilidad real
+        home_page.select_date("9") # Ajustar según disponibilidad real
         print("[DEBUG] Seleccionando primera hora disponible...")
         home_page.select_first_available_time()
         print("[DEBUG] Seleccionando primer asiento disponible...")
