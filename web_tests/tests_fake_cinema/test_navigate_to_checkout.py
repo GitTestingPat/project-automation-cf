2347d89dfee7e59bf32961f19666ec6302efed62
Patricio Andrade Gallardo <patricioandradeh@gmail.com>
2025-10-24 16:22:18 -0300

    Ajusta pruebas web

--- web_tests/tests_fake_cinema/test_navigate_to_checkout.py
+++ web_tests/tests_fake_cinema/test_navigate_to_checkout.py
@@ -19,7 +19,7 @@ def test_navigate_to_checkout(driver):
         print("[DEBUG] Navegando a detalle de Jurassic World...")
         home_page.navigate_to_movie_detail(home_page.JURASSIC_WORLD_DETAIL_BUTTON)
         print("[DEBUG] Seleccionando fecha")
-        home_page.select_date("24")  # Ajustar según disponibilidad real
+        home_page.select_date("25")  # Ajustar según disponibilidad real
         print("[DEBUG] Seleccionando primera hora disponible...")
         home_page.select_first_available_time()
         print("[DEBUG] Seleccionando primer asiento disponible...")
